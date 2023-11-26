75f99d2c0ba94a9dc585571a7c70fd6f8c97e28a
ashik-ad <dev.ashikdhimal@gmail.com>
2023-11-26 20:42:14 +0545

    chore: Prisma & auth setup

---
+++ components/nextauth-provider.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { SessionProvider } from 'next-auth/react';
+export default function NextAuthProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
