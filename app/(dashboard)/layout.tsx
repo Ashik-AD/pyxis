01a24769630f83551392f31374d023064121c591
ashik-ad <dev.ashikdhimal@gmail.com>
2023-11-27 20:02:32 +0545

    chore: add layout style

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,15 @@
-import Nav from '@/components/nav/nav';
 import { ReactNode } from 'react';
+import Nav from '@/components/nav/nav';
+import style from './style.module.css';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <main>
+    <main className={style.dashboard}>
       <Nav />
-      <div className='main__content'>{children}</div>
+      <div className={style.main__content}>{children}</div>
+      <aside>
+        <h1>Sidebar content</h1>
+      </aside>
     </main>
   );
 }
