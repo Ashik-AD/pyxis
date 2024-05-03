45c03cf93e85ea5303f86936178db3e59c4200e5
ashik-ad <dev.ashikdhimal@gmail.com>
2024-05-03 14:14:39 +0545

    chore: use mongoes from db.config and set strictQuery true

--- server/db/mongooseModels.js
+++ server/db/mongooseModels.js
@@ -1,6 +1,7 @@
-import mongoose from "mongoose";
+import { mongoose } from "../config/database.config.js";
 import { user, liked, playlistItems, playlistsList, watchList } from "./Schema.js";
 
+mongoose.set("strictQuery", true)
 export const Users = mongoose.model("users", user);
 export const Liked = mongoose.model('liked', liked);
 export const PlaylistsList = mongoose.model('playlists_lists', playlistsList);
