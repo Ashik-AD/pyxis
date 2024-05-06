b48f262e76100cdde2fc083dfcc32921ecb2ae5c
ashik-ad <dev.ashikdhimal@gmail.com>
2024-05-06 08:47:42 +0545

    chore: extract storekey type from storetype

--- frontend/src/store/storeType.ts
+++ frontend/src/store/storeType.ts
@@ -1,25 +1,13 @@
-export interface STORE_KEY {
-  store_key:
-    | 'popularMovie'
-    | 'topMovie'
-    | 'upcomingMovie'
-    | 'playingMovie'
-    | 'popularTv'
-    | 'topTv'
-    | 'playingTv'
-    | 'tv_full'
-    | 'movie_full';
-}
+import { StoreType } from "./Store";
 
-export interface STORE_ITEM_TYPE {
-  store_item_type:
-    | 'SET_POPULAR_MOVIE'
-    | 'SET_TOP_MOVIE'
-    | 'SET_UPCOMING_MOVIE'
-    | 'SET_PLAYING_MOVIE'
-    | 'SET_POPULAR_TV'
-    | 'SET_TOP_TV'
-    | 'SET_PLAYING_TV'
-    | 'SET_TV_FULL'
-    | 'SET_MOVIE_FULL';
-}
+export type STORE_KEY = keyof Omit<StoreType, "user" | "collections">;
+
+export type STORE_ITEM_TYPE =
+  | "SET_POPULAR_MOVIE"
+  | "SET_TOP_MOVIE"
+  | "SET_UPCOMING_MOVIE"
+  | "SET_PLAYING_MOVIE"
+  | "SET_POPULAR_TV"
+  | "SET_TOP_TV"
+  | "SET_PLAYING_TV"
+  | "SET_TV_FULL";
