1f22e07af4bc00dfaa644402976e4451ed82413d
ashik-ad <dev.ashikdhimal@gmail.com>
2024-05-06 08:16:04 +0545

    chore: userHook instead context

--- frontend/src/page/watchList/WatchList.tsx
+++ frontend/src/page/watchList/WatchList.tsx
@@ -1,27 +1,29 @@
-import { useContext, FC, useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import CollectionItemLists from "../../components/collection/CollectionItemLists";
 import CollectionHeading from "../../components/heading/CollectionHeading";
 import SkeletonTable from "../../components/skeleton/SkeletonTable";
 import { ax } from "../../config/default";
-import { StoreContext } from "../../store/Store";
+import useUser from "../../hooks/useUser";
 import myAppTitle from "../../utils/appTitle";
 import { noImage } from "../../utils/noImage";
+
 const WatchList: FC = () => {
   const [watchList, setWatchList] = useState<any>([]);
   const [loading, setLoading] = useState(true);
-  const {
-    store: { user },
-  } = useContext(StoreContext);
+
+  const user = useUser()
+
   useEffect(() => {
     document.title = "Pyxis - Watch List";
     return () => {
       document.title = myAppTitle();
     };
   }, []);
+
   useEffect(() => {
     let fetchWatchList = null;
     fetchWatchList = async () => {
-      const { data } = await ax.get(`${user.id}/watch-list/all`);
+      const { data } = await ax.get(`${user?.id}/watch-list/all`);
       console.log(data);
       setWatchList(data);
       setLoading(false);
@@ -30,7 +32,8 @@ const WatchList: FC = () => {
     return () => {
       fetchWatchList = null;
     };
-  }, [user.id]);
+  }, [user?.id]);
+
   return (
     <div className="">
       <CollectionHeading
