ff0924d64082559a3f76a5e52e81f9c398e0904e
ashik-ad <dev.ashikdhimal@gmail.com>
2024-05-07 08:36:32 +0545

    chore: adjust slide item

--- frontend/src/components/slider/SliderLg.tsx
+++ frontend/src/components/slider/SliderLg.tsx
@@ -16,8 +16,8 @@ const settings = {
     {
       breakpoint: 1920,
       settings: {
-        slidesToShow: 7,
-        slidesToScroll: 7,
+        slidesToShow: 5,
+        slidesToScroll: 4,
       },
     },
     {
@@ -28,16 +28,9 @@ const settings = {
       },
     },
     {
-      breakpoint: 1400,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-      },
-    },
-    {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 3,
         slidesToScroll: 2,
       },
     },
