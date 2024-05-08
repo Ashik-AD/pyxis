b7476305fd7b7f98a19c0cac839fbad3154ac02a
ashik-ad <dev.ashikdhimal@gmail.com>
2024-05-08 13:50:26 +0545

    revamp headings

--- frontend/src/components/details/headings/HeadingDesktop.tsx
+++ frontend/src/components/details/headings/HeadingDesktop.tsx
@@ -1,134 +1,221 @@
 import React, { FC, useEffect, useState } from "react";
-import { IoPlay } from "react-icons/io5";
+import { IoPlay, IoChevronBack } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { ax } from "../../../config/default";
 import formatDate from "../../../utils/formatDate";
 import { imageUrlWithSize } from "../../../utils/imageUrl";
-import ProgressCircle from "../../progress/ProgressCircle";
 import { HeadingPropTypes } from "../../types/Heading.type";
 import Styles from "../style/HeadingDesktop.module.css";
-import { RiArrowGoBackLine } from "react-icons/ri";
 import Button from "../Button";
+import Container from "../../layout/container";
+import Rating from "../../rating/rating";
+import { convertRuntime } from "../../../utils/Duration";
+import WatchListAdd from "../../watchList/WatchListAdd";
+import ItemLike from "../../like/ItemLike";
+import { BsBookmarkPlus } from "react-icons/bs";
+import DropDown from "../../dropdown/DropDown";
+import ContextPlaylist from "../../contextMenu/ContextPlaylist";
+
 const HeadingDesktop: FC<HeadingPropTypes> = (props) => {
   const {
     backdrop_path,
     title,
     type,
-    tagline,
     vote_average,
-    vote_count,
     id,
     release_date,
+    overview,
+    production_countries,
+    spoken_languages,
+    runtime,
+    poster_path,
+    genres,
   } = props;
+
   const [backdrops, setBackdrops] = useState<{ file_path: string }[]>([]);
   const [slideIndex, setSlideIndex] = useState(0);
+
   useEffect(() => {
     let fetchSlides = null;
+
     fetchSlides = async () => {
       const { data } = await ax.get(`${type}/${id}/images`);
       setBackdrops(data.backdrops.splice(0, 5));
     };
+
     fetchSlides();
+
     return () => {
       fetchSlides = null;
     };
   }, []);
+
   const handleSetSlide = (slideNum: number) => {
     setSlideIndex(slideNum);
   };
+
   return (
     <div className="hidden sm:visible">
       <div
-        className={`desktop norepeat bg-cover relative flex content-bottom px-20 lg:px-50  relative ${Styles.Heading_overlay} overflow-hidden`}
+        className={`desktop h-screen norepeat bg-cover flex content-bottom relative ${Styles.Heading_overlay} overflow-hidden`}
         style={{
-          maxHeight: 500,
-          height: 450,
           background: `url(${imageUrlWithSize(
             slideIndex === 0 ? backdrop_path : backdrops[slideIndex].file_path,
-            "1280"
+            "1280",
           )})`,
-          zIndex: 9999,
           transition: "background 1s ease",
         }}
       >
-        <div className="flex flex-col w-full space-between relative z-1 max-w-1250 h-300 pb-50">
-          <div className="flex space-between w-full align-center ">
-            <div className={`flex flex-col`}>
-              <div
-                className="flex gap-20 font-bold color-white"
-                style={{ fontSize: 25 }}
-              >
-                <Link to="../">
-                  <Button styles="border-1 hover-bg-fade">
-                    <RiArrowGoBackLine />
+        <Container>
+          <div className="flex w-full content-bottom relative z-1 pb-50">
+            <div className="flex space-between align-center">
+              <div className={`flex flex-col gap-30`}>
+                <div className="flex flex-col gap-20 color-white">
+                  <Link to="../">
+                    <Button styles="font-semibold text-small">
+                      <>
+                        <IoChevronBack className="text-sm font-bold" />
+                        <span className="text-regular">Go Back</span>
+                      </>
+                    </Button>
+                  </Link>
+                  <div className="genre_list flex wrap gap-10 my-10">
+                    {genres.map((el: any) => (
+                      <span
+                        className="px-10 py-4 text-sm font-bold border-2 border-gray"
+                        key={el.id}
+                        style={{
+                          borderTopRightRadius: 12,
+                          borderBottomLeftRadius: 12,
+                        }}
+                      >
+                        {el.name}
+                      </span>
+                    ))}
+                  </div>
+                  <div className="flex flex-col gap-40">
+                    <h1 className="font-semibold text-heading">{title}</h1>
+                    <Rating rating={vote_average} />
+                  </div>
+                  <p className="font-medium lg:w-75">{overview}</p>
+                </div>
+                <div
+                  className="grid col-2 gap-10"
+                  style={{ width: "min-content" }}
+                >
+                  <span className="color-gray font-medium">Country:</span>
+                  <span className="color-white font-semibold word-nowrap">
+                    {production_countries.map((country) => country.name)}
+                  </span>
+                  <span className="color-gray font-medium">Language:</span>
+                  <span className="color-white font-semibold word-nowrap">
+                    {spoken_languages.map((lang) => lang.english_name + ", ")}
+                  </span>
+                  <span className="color-gray font-medium">Duration:</span>
+                  <span className="color-white font-semibold word-nowrap">
+                    {convertRuntime(runtime)}
+                  </span>
+                  <span className="color-gray font-medium">Released:</span>
+                  <span className="color-white font-semibold word-nowrap">
+                    {formatDate(release_date)}
+                  </span>
+                </div>
+                <div className="flex items-center gap-20">
+                  <WatchListAdd
+                    average_vote={vote_average}
+                    duration={runtime}
+                    media_type={type}
+                    item_key={id}
+                    release_date={release_date}
+                    title={title}
+                    poster_path={poster_path}
+                    styles={{
+                      background: "#a30254",
+                      border: 0,
+                      padding: "16px 40px",
+                      fontSize: 17,
+                    }}
+                  />
+                  <ItemLike
+                    color="#ca2845"
+                    id={id}
+                    duration={runtime}
+                    posterPath={poster_path}
+                    title={title}
+                    release_date={release_date}
+                    media_type="movie"
+                  />
+                  <Button
+                    color="#ca2845"
+                    styles="drp-collection relative rounded-full"
+                  >
+                    <>
+                      <BsBookmarkPlus className="drp-collection" />
+                      <div className="absolute bg-primary">
+                        <DropDown label="" drpId="drp-collection">
+                          <ContextPlaylist
+                            playlistItemId={id}
+                            playlistItemName={title}
+                            posterURL={poster_path ? poster_path : ""}
+                            releaseDate={release_date}
+                            duration={runtime}
+                            mediaType="movie"
+                          />
+                        </DropDown>
+                      </div>
+                    </>
                   </Button>
-                </Link>
-                <div className="flex flex-col">
-                  <span>{title}</span>
-                  {tagline && (
-                    <span className="color-white text-regular font-bold my-6">
-                      &quot;{tagline}&quot;
-                    </span>
-                  )}
                 </div>
               </div>
             </div>
-            <article className="flex flex-col space-y-10 align-center color-white">
-              <span>
-                <span className="font-semibold uppercase bg-primary px-20 py-6 rounded-xlg">
-                  {type}
-                </span>
-              </span>
-              <span className="capitalize font-bold text-medium">
-                {formatDate(release_date)}
-              </span>
-            </article>
-          </div>
-          <div className="flex space-between align-center color-white">
-            <div className="flex gap-10 align-center">
-              <ProgressCircle
-                radius={50}
-                strokeWidth={15}
-                value={vote_average}
-                labelStyles="text-lg"
-              />
-              <article className="flex flex-col font-bold">
-                <span className="uppercase text-sm">
-                  {vote_count > 1000
-                    ? `${Math.floor(vote_count / 1000)}K votes`
-                    : `${vote_count} Votes`}
-                </span>
-                {vote_count > 500 && vote_average > 6 && (
-                  <span className="text-xsm bg-light">
-                    Our Users Are Recommending It
-                  </span>
-                )}
-              </article>
-            </div>
-            <div className="flex gap-20 align-center">
-              <Link
-                to={`trailer/${id}`}
-                className="flex gap-20 color-white font-bold border-1  align-center px-20 py-14 rounded-xxlg border-gray"
-              >
-                <IoPlay color="purple" /> <span className="px-10">TRAILER</span>
-              </Link>
-            </div>
-            <div className="flex align-center gap-10">
-              {backdrops.map((el: any, index: number) => (
-                <span
-                  key={index}
-                  className="overflow-hidden rounded-lg hover-shadow "
-                  onClick={() => handleSetSlide(index)}
+            <div className="flex flex-col gap-30 color-white">
+              <div className="relative overflow-hidden">
+                <img
+                  src={imageUrlWithSize(poster_path, "342")}
+                  className="w-full h-250"
+                  alt={title}
+                  style={{
+                    objectFit: "cover",
+                    objectPosition: "center",
+                    opacity: 0.8,
+                  }}
+                />
+                <div
+                  className="absolute flex content-bottom left-0 top-0 w-full h-full"
+                  style={{
+                    background:
+                      "linear-gradient(transparent, #060034 80%)",
+                  }}
                 >
-                  <img
-                    src={imageUrlWithSize(el.file_path, "1280")}
-                    className="rounded-lg md:w-40px lg:w-70px hover-scaleup transition"
-                  />
-                </span>
-              ))}
+                  <div className="h-half w-full flex flex-col content-center space-between pb-20">
+                    <span>
+                      <Button styles="bg-fade rounded-full">
+                        <IoPlay className="text-heading color-black" />
+                      </Button>
+                    </span>
+                    <span className="font-semibold text-center text-medium">
+                      Watch Trailer
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="flex align-center gap-10">
+                {backdrops.map((el: any, index: number) => (
+                  <span
+                    key={index}
+                    className="overflow-hidden rounded-lg hover-shadow "
+                    onClick={() => handleSetSlide(index)}
+                  >
+                    <img
+                      src={imageUrlWithSize(el.file_path, "1280")}
+                      className="rounded-lg md:w-40px lg:w-70px hover-scaleup transition"
+                    />
+                  </span>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
+        </Container>
       </div>
     </div>
   );
--- frontend/src/components/details/headings/HeadingMobile.tsx
+++ frontend/src/components/details/headings/HeadingMobile.tsx
@@ -9,6 +9,7 @@ import formatDate from "../../../utils/formatDate";
 import { convertRuntime } from "../../../utils/Duration";
 import Button from "../Button";
 import { BsBookmarkPlus } from "react-icons/bs";
+import Container from "../../layout/container";
 
 const HeadingMobile: FC<HeadingPropTypes> = (props) => {
   const {
@@ -19,94 +20,100 @@ const HeadingMobile: FC<HeadingPropTypes> = (props) => {
     release_date,
     id,
     runtime,
+    production_countries,
+    spoken_languages,
+    overview,
   } = props;
   return (
-    <div
-      className="relative visible sm:hidden"
-      style={{ zIndex: 999, minHeight: "100%" }}
-    >
+    <div className="visible sm:hidden">
       <div
-        className="top-0 left-0 w-full bg-cover relative no-repeat"
+        className="h-screen flex content-bottom top-0 left-0 bg-cover relative no-repeat pb-50"
         style={{
-          backgroundImage: `url(${imageUrlWithSize(poster_path, "original")})`,
-          minHeight: "400px",
+          backgroundImage: `url(${imageUrlWithSize(poster_path, "780")})`,
+          backgroundPosition: "center",
         }}
       >
         <div
           className="absolute h-full w-full top-0 left-0"
-          style={{ background: "linear-gradient(0deg, #060034, transparent)" }}
+          style={{ background: "linear-gradient(transparent, #000 90%)" }}
         ></div>
-      </div>
-      <div
-        className="w-full bottom-0 left-0 flex flex-col gap-10 px-20"
-        style={{
-          background: "linear-gradient(#060034, rgb(0 0 0 / 94%) 100%)",
-        }}
-      >
-        <span className="color-white text-lg font-bold">{title}</span>
-        <div
-          className="flex gap-10 nowrap color-white font-semibold text-xsm overflow-x-scroll scrollbar-on-hover"
-          style={{ marginBottom: 5 }}
-        >
-          {genres.map((el: { id: number; name: string }) => (
-            <span
-              key={el.id}
-              style={{ paddingTop: 3, paddingBottom: 3 }}
-              className="bg-dark-fade px-6 rounded-regular border-1 border-success"
+        <Container className="z-1">
+          <div className="w-full bottom-0 left-0 flex flex-col gap-20">
+            <span className="color-white text-xlg font-bold">{title}</span>
+            <div className="flex gap-10 nowrap color-white font-semibold text-xsm overflow-x-scroll scrollbar-on-hover">
+              {genres.map((el: { id: number; name: string }) => (
+                <span
+                  key={el.id}
+                  style={{ paddingTop: 3, paddingBottom: 3, borderTopRightRadius: 12 }}
+                  className="bg-dark-fade px-6 rounded-regular border-1 border-gray"
+                >
+                  {el.name}
+                </span>
+              ))}
+            </div>
+            <p className="text-sm font-semibold">{overview}</p>
+            <div
+              className="grid col-2 gap-10 my-20 "
+              style={{ width: "min-content" }}
             >
-              {el.name}
-            </span>
-          ))}
-        </div>
-        <div className="flex gap-10 color-gray font-semibold text-semi uppercase">
-          <span>{formatDate(release_date)?.split(" ").pop()}</span>•
-          <span>{convertRuntime(runtime)} </span>
-        </div>
-        <div className="flex content-center gap-10" style={{ marginTop: 30 }}>
-          <WatchListAdd
-            title={title}
-            item_key={id}
-            average_vote={vote_average}
-            media_type="movie"
-            poster_path={poster_path}
-            release_date={release_date}
-            duration={runtime}
-            styles={{ backgroundColor: "#CA2845", border: 0, width: "70%" }}
-            classNames="shadow-lg text-regular font-medium letter-space-1 px-20 py-16 content-center"
-          />
-        </div>
-      </div>
-      <div className="absolute flex gap-10 top-0 right-0 py-20 px-20 color-white">
-        <span>
-          <ItemLike
-            color="#CA2845"
-            id={id}
-            duration={runtime}
-            posterPath={poster_path}
-            title={title}
-            release_date={release_date}
-            media_type="movie"
-          />
-        </span>
-        <span>
-          <Button color={"#CA2845"} styles="drp-collection">
-            <>
-              <BsBookmarkPlus className="drp-collection" />
-              <div className="absolute">
-                <DropDown label="" drpId="drp-collection">
-                  <ContextPlaylist
-                    playlistItemId={id}
-                    playlistItemName={title}
-                    posterURL={poster_path ? poster_path : ""}
-                    releaseDate={release_date}
-                    duration={runtime}
-                    mediaType="movie"
-                  />
-                </DropDown>
-              </div>
-            </>
-          </Button>
-        </span>
+              <span className="color-gray font-medium text-sm">Country:</span>
+              <span className="color-white font-semibold word-nowrap text-sm">
+                {production_countries.map((country) => country.name)}
+              </span>
+              <span className="color-gray font-medium text-sm">Language:</span>
+              <span className="color-white font-semibold word-nowrap text-sm">
+                {spoken_languages.map((lang) => lang.english_name + ", ")}
+              </span>
+              <span className="color-gray font-medium text-sm">Duration:</span>
+              <span className="color-white font-semibold word-nowrap text-sm">
+                {convertRuntime(runtime)}
+              </span>
+              <span className="color-gray font-medium text-sm">Released:</span>
+              <span className="color-white font-semibold word-nowrap text-sm">
+                {formatDate(release_date)}
+              </span>
+            </div>
+            <div className="flex content-center gap-10 ">
+              <WatchListAdd
+                title={title}
+                item_key={id}
+                average_vote={vote_average}
+                media_type="movie"
+                poster_path={poster_path}
+                release_date={release_date}
+                duration={runtime}
+                styles={{ backgroundColor: "#CA2845", border: 0, width: "70%" }}
+                classNames="shadow-lg text-regular font-medium letter-space-1 px-20 py-16 content-center"
+              />
+              <ItemLike
+                color="#CA2845"
+                id={id}
+                duration={runtime}
+                posterPath={poster_path}
+                title={title}
+                release_date={release_date}
+                media_type="movie"
+              />
+              <Button color={"#CA2845"} styles="drp-collection rounded-full">
+                <>
+                  <BsBookmarkPlus className="drp-collection" />
+                  <div className="absolute">
+                    <DropDown label="" drpId="drp-collection">
+                      <ContextPlaylist
+                        playlistItemId={id}
+                        playlistItemName={title}
+                        posterURL={poster_path ? poster_path : ""}
+                        releaseDate={release_date}
+                        duration={runtime}
+                        mediaType="movie"
+                      />
+                    </DropDown>
+                  </div>
+                </>
+              </Button>
+            </div>
+          </div>
+        </Container>
       </div>
     </div>
   );
