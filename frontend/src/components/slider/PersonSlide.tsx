fe07299938abe7e196ea496896449e5fe6866664
ashik-ad <dev.ashikdhimal@gmail.com>
2024-05-08 15:49:29 +0545

    chore: remove slider and revamp design

--- frontend/src/components/slider/PersonSlide.tsx
+++ frontend/src/components/slider/PersonSlide.tsx
@@ -1,61 +1,9 @@
 import React from "react";
 
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-const SliderArrow = React.lazy(() => import("./SliderArrow"));
-
 import useFetch from "../../hooks/useFetch";
 import { imageUrlWithSize } from "../../utils/imageUrl";
 import PersonCard from "../cards/PersonCard";
-export const settings = {
-  dots: false,
-  responsive: [
-    {
-      breakpoint: 1920,
-      settings: {
-        slidesToShow: 12,
-        slidesToScroll: 12,
-      },
-    },
-    {
-      breakpoint: 1620,
-      settings: {
-        slidesToShow: 10,
-        slidesToScroll: 10,
-      },
-    },
-    {
-      breakpoint: 1400,
-      settings: {
-        slidesToShow: 5,
-        slidesToScroll: 5,
-        infinite: true,
-      },
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-      },
-    },
-    {
-      breakpoint: 670,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-      },
-    },
-    {
-      breakpoint: 464,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-  ],
-};
+
 const PersonSlide: React.FC<PropTypes> = (props) => {
   let { data, loading, error } = useFetch(props.url);
   if (loading) return <h1>Loading</h1>;
@@ -65,17 +13,13 @@ const PersonSlide: React.FC<PropTypes> = (props) => {
     data = props.items;
   }
   return (
-    <Slider
-      {...settings}
-      nextArrow={<SliderArrow />}
-      prevArrow={<SliderArrow />}
-    >
+    <div className="grid col-2  md:col-3 gap-20 row-gap-30">
       {data.map((el: any) => (
         <PersonCard
           id={el.id}
           person_name={el.name}
           profile_img={
-            el.profile_path ? imageUrlWithSize(el.profile_path, "154") : ""
+            el.profile_path ? imageUrlWithSize(el.profile_path, "185") : ""
           }
           character={el.character}
           color={props.color}
@@ -83,7 +27,7 @@ const PersonSlide: React.FC<PropTypes> = (props) => {
           gender={el.gender}
         />
       ))}
-    </Slider>
+    </div>
   );
 };
 interface PropTypes {
