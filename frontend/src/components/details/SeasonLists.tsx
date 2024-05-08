70380f0e78bf230bf268980279909a51dc83dc57
ashik-ad <dev.ashikdhimal@gmail.com>
2024-05-08 15:47:46 +0545

    chore: revamp design

--- frontend/src/components/details/SeasonLists.tsx
+++ frontend/src/components/details/SeasonLists.tsx
@@ -1,45 +1,45 @@
 import React from "react";
-import { RiArrowRightLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
-import formatDate from "../../utils/formatDate";
-import { imageUrl } from "../../utils/imageUrl";
+import { imageUrlWithSize } from "../../utils/imageUrl";
 import { noImage } from "../../utils/noImage";
 import Image from "../img/Image";
 
 const SeasonLists: React.FC<PropTypes> = ({ season_lists }) => {
   if (season_lists.length <= 0) return null;
   return (
-    <div className="grid col-1 sm:col-2 gap-20">
+    <div className="grid col-1 sm:col-3 gap-20">
       {season_lists.map((el: items) => (
         <Link
           to={`season/${el.season_number}`}
           key={el.id}
-          className="flex align-bottom gap-20"
+          className="relative flex align-bottom gap-20 w-300 sm:w-250 lg:w-300"
         >
-          <span className="img_container">
+          <span className="img_container sm:w-full">
             <Image
-              className="rounded-lg"
-              src={el.poster_path ? imageUrl(el.poster_path) : noImage.default}
+              className="rounded-lg w-full"
+              src={
+                el.poster_path
+                  ? imageUrlWithSize(el.poster_path, "300")
+                  : noImage.default
+              }
               alt={el.name}
-              styles={{ width: 100 }}
             />
           </span>
-          <div className="meta flex flex-col gap-10 py-10 color-white">
-            <span className="text-medium font-semibold truncate">
-              {el.name}
-            </span>
-            <span className="font-bold text-sm color-gray">
-              {formatDate(el.air_date)}
-            </span>
-            <span className=" font-semibold text-sm">
-              {el.episode_count} Episodes
-            </span>
-            <Link
-              to={`season/${el.season_number}`}
-              className="color-gray font-bold text-lg color-gray"
-            >
-              <RiArrowRightLine />
-            </Link>
+          <div
+            className="absolute bottom-0 left-0 w-full h-full flex content-bottom gap-10 py-10 p-20 pb-50 rounded-lg"
+            style={{
+              background: "linear-gradient(transparent 30%, #00072c 90%)",
+              backdropFilter: "blur(0.8px)",
+            }}
+          >
+            <div className="w-full flex flex-col gap-10">
+              <span className="text-lg font-semibold truncate color-white">
+                {el.name}
+              </span>
+              <span className=" font-semibold text-sm color-gray">
+                {el.episode_count} Episodes
+              </span>
+            </div>
           </div>
         </Link>
       ))}
