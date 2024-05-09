d80a6d62f469a09eac5077a69ca519765cc01bae
ashik-ad <dev.ashikdhimal@gmail.com>
2024-05-09 10:16:20 +0545

    add renderPersons with suppressed ts error

--- frontend/src/components/details/Crew.tsx
+++ frontend/src/components/details/Crew.tsx
@@ -2,41 +2,32 @@ import React from "react";
 import useFetch from "../../hooks/useFetch";
 import selectedPeople from "../../utils/selectedPeople";
 import { imageUrlWithSize } from "../../utils/imageUrl";
-import { PersonCardTypes } from "../types/Person.type";
-import PersonCard from "../cards/PersonCard";
 import { noImage } from "../../utils/noImage";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { RenderPersons } from "../person-list";
 
-const Crew: React.FC<PropTypes> = ({ color, id, type }) => {
+const Crew: React.FC<PropTypes> = ({ id, type }) => {
   const { data, loading, error } = useFetch(`${type}/credit/${id}/crew`);
   if (loading) return <h1>Loading...</h1>;
   if (error) return <h1>Something went wrong</h1>;
   if (!data) return <h1>No data</h1>;
 
-  const crew: PersonCardTypes[] = selectedPeople(data).map(
-    (el: any): PersonCardTypes => ({
-      id: el.id,
-      person_name: el.original_name,
-      profile_img: el.profile_path
-        ? imageUrlWithSize(el.profile_path, "185")
-        : el.gender === 2
-          ? noImage.male
-          : noImage.female,
-      department: el.job,
-      color: color,
-      gender: el.gender,
-    }),
-  );
+  const crew = selectedPeople(data).map((el) => ({
+    id: el.id,
+    name: el.original_name,
+    profile_path: el.profile_path
+      ? imageUrlWithSize(el.profile_path, "185")
+      : el.gender === 2
+        ? noImage.male
+        : noImage.female,
+    department: el.job,
+    gender: el.gender,
+  }));
 
-  return (
-    <div className="grid col-2 md:col-3 gap-20 row-gap-30">
-      {crew.map((el: any) => (
-        <PersonCard {...el} color={color} key={el.id} />
-      ))}
-    </div>
-  );
+  //@ts-ignore
+  return <RenderPersons list={crew} />;
 };
 interface PropTypes {
   color: string;
