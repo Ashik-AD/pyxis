86f9197f0e9deb53072bb5ae72cf4bd0278e4c2e
ashik-ad <dev.ashikdhimal@gmail.com>
2024-05-09 10:13:43 +0545

    chore: remove padding & medium font

--- frontend/src/components/cards/Title.tsx
+++ frontend/src/components/cards/Title.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const Title: React.FC<TitlePropTypes> = ({ title, link }) => {
   return (
-    <div className='flex space-between items-center color-white py-10 sm:py-20 px-10'>
-      <span className='text-regular sm:text-lg font-bold'>{title}</span>
+    <div className='flex space-between items-center color-white py-10 sm:py-20'>
+      <span className='text-regular sm:text-lg font-semibold'>{title}</span>
       {link && (
         <Link to={link} className='text-sm font-semibold color-info'>
           See more
