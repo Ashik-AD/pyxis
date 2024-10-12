9801c27d78ecdb04b5d7588cde2aaec346688d5d
ashik-ad <dev.ashikdhimal@gmail.com>
2024-10-12 21:33:40 +0545

    fixed: alert box color & added color for danger status

--- frontend/src/components/alert/AlertText.tsx
+++ frontend/src/components/alert/AlertText.tsx
@@ -1,13 +1,22 @@
 import { FC } from "react";
+import { TiWarning } from "react-icons/ti";
+import { AiFillInfoCircle } from "react-icons/ai";
 
 const AlertText: FC<PropsType> = ({ text, type }) => {
   return (
     <div
-      className={`error absolute bottom-20 flex ${
-        type === "info" ? "bg-info color-black" : "bg-primary"
+      className={`error flex gap-10 items-center absolute bottom-20 flex ${
+        type === "info" ? "bg-info" : "bg-danger"
       } color-white py-10 px-20 rounded-lg text-regular font-semibold`}
       style={{ zIndex: 9999 }}
     >
+      <i className="flex">
+        {type == "warn" ? (
+          <TiWarning size={24} />
+        ) : (
+          <AiFillInfoCircle size={24} />
+        )}
+      </i>
       {text}
     </div>
   );
