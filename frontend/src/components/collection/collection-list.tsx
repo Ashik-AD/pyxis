f7753d753d61385e01ce97e534358500f53b20ac
ashik-ad <dev.ashikdhimal@gmail.com>
2024-10-14 22:55:38 +0545

    renamed

--- frontend/src/components/collection/collection-list.tsx
+++ frontend/src/components/collection/collection-list.tsx
@@ -1,46 +1,38 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import AlertShow from "../../context/AlertContext";
 import useStore from "../../hooks/useStore";
 import CollectionCard from "./components/CollectionCard";
 import UpdateCollection from "./UpdateCollection";
 
-const Collections = () => {
+const CollectionList = () => {
   let { user, collections } = useStore();
   const [updatModal, setUpdateModal] = useState(false);
   let routeState: any = useLocation().state;
-
-  useEffect(() => {
-    if (routeState && collections) {
-      if (routeState["playlist_id"]) {
-        setUpdateModal(true);
-      }
-    }
-  }, [routeState]);
-
-  useEffect(() => {
-    return () => clearRouteState();
-  }, []);
 
   const clearRouteState = () => {
     routeState = {};
     setUpdateModal(false);
   };
+
   return (
-    <section className="grid col-2 sm:col-5 gap-20 py-50 px-10 sm:px-50">
-      <AlertShow>
-        {collections &&
-          user &&
-          collections.map((collection) => (
-            <CollectionCard
-              full_name={user?.full_name!}
-              key={collection.playlist_id}
-              uid={user?.id!}
-              playlist_id={collection.playlist_id}
-              playlist_name={collection.playlist_name}
-            />
-          ))}
-      </AlertShow>
+    <section className="flex flex-col gap-20">
+      <section className="grid col-2 sm:col-5 gap-10">
+        <AlertShow>
+          {collections &&
+            user &&
+            collections.map((collection) => (
+              <CollectionCard
+                full_name={user?.full_name!}
+                key={collection.playlist_id}
+                uid={user?.id!}
+                playlist_id={collection.playlist_id}
+                playlist_name={collection.playlist_name}
+                totalItem={collection.total_item}
+              />
+            ))}
+        </AlertShow>
+      </section>
       {updatModal && (
         <UpdateCollection
           collection_id={routeState.playlist_id ? routeState.playlist_id : ""}
@@ -55,4 +47,4 @@ const Collections = () => {
   );
 };
 
-export default Collections;
+export default CollectionList;
