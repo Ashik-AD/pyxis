d49df625d8ba54730268253c2eddb309cee64e2a
ashik-ad <dev.ashikdhimal@gmail.com>
2024-10-14 22:59:54 +0545

    fixed: collection item type

--- frontend/src/components/collection/collection.type.ts
+++ frontend/src/components/collection/collection.type.ts
@@ -1,18 +1,12 @@
-interface CollectionType {
-  playlist_items_id: string;
-  playlist_id: string;
+export type CollectionItem = {
   uid: string;
-  items_name: string;
-  poster_url: string;
-  added_date: string;
-  duration: number;
-  media_type: string;
-  released_date: Date | null;
-  is_liked: 'true' | 'false' | null;
-  liked_id?: string;
-  title?: string;
-  liked_date?: Date | null;
-  item_key?: string;
   id: string;
-}
-export default CollectionType;
+  added_date: Date | string;
+  duration: number;
+  is_liked: boolean;
+  title: string;
+  media_type: "movie" | "tv";
+  playlist_id: string;
+  poster_url: string;
+  released_date: Date;
+};
