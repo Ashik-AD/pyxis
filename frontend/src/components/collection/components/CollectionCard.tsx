de36a430bdaa4012a6d2686f2ce4dc7640185411
ashik-ad <dev.ashikdhimal@gmail.com>
2024-10-14 23:01:51 +0545

    fixed: collection dropdown

--- frontend/src/components/collection/components/CollectionCard.tsx
+++ frontend/src/components/collection/components/CollectionCard.tsx
@@ -1,33 +1,21 @@
-import React, { FC } from "react";
 import { Link } from "react-router-dom";
 import { noImage } from "../../../utils/noImage";
 import CollectionDrpDown from "../dropdown/CollectionDrpDown";
-import { IoEllipsisHorizontalSharp } from "react-icons/io5";
-import CollectionContextMenu from "../../contextMenu/CollectionContextMenu";
 
-const CollectionCard: FC<PropsType> = (props) => {
-  const { uid, playlist_id, playlist_name, full_name } = props;
-  const contextRef = React.createRef<HTMLDivElement>();
-  const hanldeContextMenu = (eve: React.SyntheticEvent) => {
-    eve.preventDefault();
-    document
-      .querySelectorAll(".collection-context-menu")
-      .forEach((el) => el.classList.add("hidden"));
-    contextRef.current?.classList.toggle("hidden");
-  };
-  window.addEventListener("click", () => {
-    contextRef.current?.classList.add("hidden");
-  });
+interface Props {
+  playlist_id: string;
+  playlist_name: string;
+  full_name: string;
+  uid: string;
+  totalItem: number;
+}
+
+const CollectionCard = (props: Props) => {
+  const { uid, playlist_id, playlist_name, full_name, totalItem } = props;
+
   return (
-    <div
-      key={playlist_id}
-      className="relative flex flex-col bg-secondary p-6 lg:p-16 rounded-lg overflow-hidden z-0 visible-on-hover"
-      onContextMenu={hanldeContextMenu}
-    >
-      <div className="content absolute z-2 right-10 top-0 cursor-normal px-6 py-10 color-gray visible-on-hover">
-        <IoEllipsisHorizontalSharp
-          className={`text-lg col0r-white cursor-pointer child-hover drp-collection-${playlist_id}`}
-        />
+    <div className="relative flex flex-col bg-secondary p-6 lg:p-16 rounded-lg overflow-hidden z-0 visible-on-hover">
+      <div className="child-hover absolute z-2 right-20 top-20 cursor-normal px-6 py-10 color-gray">
         <CollectionDrpDown
           drp_id={`drp-collection-${playlist_id}`}
           collection_id={playlist_id}
@@ -48,22 +36,14 @@ const CollectionCard: FC<PropsType> = (props) => {
       >
         {playlist_name}
       </span>
-      <span className="color-gray my-6">By {full_name.split(" ").shift()}</span>
-      <div
-        className="collection-context-menu absolute right-10 bottom-10 hidden"
-        ref={contextRef}
-      >
-        <CollectionContextMenu collection_id={playlist_id} uid={uid} />
+      <div className="flex gap-10 space-between">
+        <span className="color-gray my-6">
+          By {full_name.split(" ").shift()}
+        </span>
+        <span className="color-gray my-6">{totalItem || 0} item(s)</span>
       </div>
     </div>
   );
 };
-
-interface PropsType {
-  playlist_id: string;
-  playlist_name: string;
-  full_name: string;
-  uid: string;
-}
 
 export default CollectionCard;
