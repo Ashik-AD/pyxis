14e61a654fa3826bb7dc169581d1ed358574d1a1
ashik-ad <dev.ashikdhimal@gmail.com>
2024-10-14 22:19:39 +0545

    refactor: replace context with dispatch hook

--- frontend/src/components/contextMenu/item/RemovedFromLiked.tsx
+++ frontend/src/components/contextMenu/item/RemovedFromLiked.tsx
@@ -1,34 +1,35 @@
-import { FC, useContext, useState } from "react";
+import { FC, useState } from "react";
 import { ax } from "../../../config/default";
 import { ItemPropsTypes } from "./itemTypes";
 import ShowAlert from "../../alert/ShowAlert";
-import { CollectionContext } from "../../collection/CollectionItemLists";
+import useDispatch from "../../../hooks/useDispatch";
 const RemovedFromLiked: FC<PropsType> = ({
   uid,
   liked_id,
   handleClick,
-  id,
   handleMouseOver,
 }) => {
+  const dispatch = useDispatch();
   const [success, setSuccess] = useState("");
   const [error, setError] = useState("");
-  const ctx = useContext(CollectionContext);
+
   const handleRemoveLiked = async () => {
     try {
       const { data } = await ax.post(`${uid}/liked/delete`, {
         data: { likedId: liked_id },
       });
+      dispatch({ type: "REMOVE_LIKED", payload: liked_id });
       setSuccess(data);
-      ctx.updateCollection(id);
     } catch (error) {
-      setError("Cant removed from your liked");
-      console.log(error);
+      setError("Can't removed from liked");
     }
   };
+
   const clearAlert = () => {
     setSuccess("");
     setError("");
   };
+
   return (
     <div
       className="hover-bg-fade py-10 px-16 rounded-regular"
@@ -38,7 +39,7 @@ const RemovedFromLiked: FC<PropsType> = ({
         handleClick && handleClick();
       }}
     >
-      Remove from your liked
+      Remove from like
       <ShowAlert success={success} error={error} clearAlert={clearAlert} />
     </div>
   );
