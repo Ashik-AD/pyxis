db64fb716269b85f3693096690811f814df95ea0
ashik-ad <dev.ashikdhimal@gmail.com>
2024-10-14 22:38:34 +0545

    refactor: fetch collection by id

--- frontend/src/components/collection/CollectionInfo.tsx
+++ frontend/src/components/collection/CollectionInfo.tsx
@@ -1,36 +1,42 @@
-import { FC, useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import UpdateCollection from "./UpdateCollection";
 import { ax } from "../../config/default";
 import CollectionItemLists from "./CollectionItemLists";
 import useUser from "../../hooks/useUser";
+import { CollectionItem } from "./collection.type";
 
-const CollectionInfo: FC = () => {
-  let { pathname } = useLocation();
-  const playlist_id = pathname.split("/").pop();
-  const [collectionItems, setCollectionItems] = useState<any>([]);
+const CollectionInfo = () => {
+  let params = useParams();
+  const [collectionItems, setCollectionItems] = useState<CollectionItem[]>([]);
 
   const [updateCollection, setUpdateCollection] = useState(false);
   const user = useUser();
 
   const fetchCollectionItems = async () => {
-    const { data } = await ax.get(`collection/${user?.id}/${playlist_id}/all`);
+    const { data } = await ax.get(`collection/${user?.id}/${params?.id}/all`);
     setCollectionItems(data);
   };
 
   useEffect(() => {
     fetchCollectionItems();
-  }, [playlist_id]);
+  }, [params?.id]);
+
+  function handleCollection(items?: CollectionItem[]) {
+    if (items) {
+      setCollectionItems(items);
+    }
+  }
 
   return (
     <section className="w-full h-full">
       <CollectionItemLists
         items={collectionItems}
-        handleCollection={setCollectionItems}
+        handleCollection={handleCollection}
       />
       {updateCollection && (
         <UpdateCollection
-          collection_id={playlist_id ? playlist_id : ""}
+          collection_id={params?.id || ""}
           handleClick={() => setUpdateCollection(false)}
         />
       )}
