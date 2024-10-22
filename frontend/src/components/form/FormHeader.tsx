2fb3d2d903596d71e1db8e19cd37438da9d10ecb
ashik-ad <dev.ashikdhimal@gmail.com>
2024-10-22 23:48:59 +0545

    refactored form elements

--- frontend/src/components/form/FormHeader.tsx
+++ frontend/src/components/form/FormHeader.tsx
@@ -6,12 +6,12 @@ type Props = {
 };
 const FormHeader = ({ title, subtitle, className }: Props) => (
   <div
-    className={`form-head mb-50 color-white cursor-default z-2 ${
+    className={`form-head flex flex-col gap-10 text-center mb-50 color-white cursor-default z-2 ${
       className ? className : ""
     }`}
   >
-    <h1 className="text-xlg font-semibold">{title}</h1>
-    {subtitle && <span className="text-regular font-medium">{subtitle}</span>}
+    <h1 className="text-heading font-semibold">{title}</h1>
+    {subtitle && <span className="text-medium font-medium color-fade">{subtitle}</span>}
   </div>
 );
 export default React.memo(FormHeader);
--- frontend/src/components/form/FormWrapper.tsx
+++ frontend/src/components/form/FormWrapper.tsx
@@ -1,15 +1,9 @@
 import { memo } from "react";
-import { noImage } from "../../utils/noImage";
 
 const FormWrapper = (props: any) => {
   return (
-    <div
-      className="form_container flex h-screen content-center w-screen bg-cover z-2 overflow-hidden"
-      style={{
-        background: `linear-gradient(157.41deg, rgba(14, 156, 2, 0.75) 14.4%, rgba(173, 11, 187, 0.6375) 89.92%),url(${noImage.banner})`,
-      }}
-    >
-      <div className="w-full h-full sm:h-auto md:w-75 lg:w-40 px-20 sm:px-50 py-50 bg-secondary overflow-hidden sm:rounded-lg">
+    <div className="form_container flex content-center bg-cover overflow-hidden">
+      <div className="w-full h-full sm:h-auto px-20 sm:px-60 py-50 overflow-hidden sm:rounded-lg">
         {props.children}
       </div>
     </div>
