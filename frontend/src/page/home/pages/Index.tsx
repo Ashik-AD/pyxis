f5b475dc799264e566764e8de0c19aa73e4f0ccb
ashik-ad <dev.ashikdhimal@gmail.com>
2024-10-22 23:51:38 +0545

    removed document title

--- frontend/src/page/home/pages/Index.tsx
+++ frontend/src/page/home/pages/Index.tsx
@@ -1,15 +1,11 @@
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import { Routes, Route } from "react-router-dom";
-import myAppTitle from "../../../utils/appTitle";
 import NotFound from "../../404/NotFound";
 import MovieContainer from "./MovieContainer";
 import Trailer from "./Trailer";
 import TvContainer from "./TvContainer";
 
 const Index: FC = () => {
-  useEffect(() => {
-    document.title = myAppTitle();
-  }, []);
   return (
     <section className="relative flex flex-col bg-black">
       <Routes>
