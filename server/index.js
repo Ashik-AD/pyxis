32b088ca6870199e5d69eb4c210f969b93c4c3a3
ashik-ad <dev.ashikdhimal@gmail.com>
2024-10-22 20:41:42 +0545

    configured deployment on vercel

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
-import http from "http";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 import route from "./routes/route.js";
-import { initDatabaseConnection } from './config/database.config.js'
+import { initDatabaseConnection } from "./config/database.config.js";
 dotenv.config();
 
 const app = express();
@@ -13,8 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 route(app);
-const server = http.createServer(app);
 const PORT = process.env.PORT || 6969;
 
 initDatabaseConnection();
-server.listen(PORT, console.log(`Server is running on Port: ${PORT}`));
+app.listen(PORT, console.log(`Server is running on Port: ${PORT}`));
+export default app;
