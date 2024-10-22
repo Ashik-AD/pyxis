70ef2811cac9d07418cfbce67077609f0e8a4940
ashik-ad <dev.ashikdhimal@gmail.com>
2024-10-22 21:23:17 +0545

    testing cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import bodyParser from "body-parser";
 import route from "./routes/route.js";
+import cors from 'cors'
 import { initDatabaseConnection } from "./config/database.config.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors())
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "POST, GET, HEAD, UPDATE, PUT, DELETE",
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+//   );
+//   next();
+// });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
