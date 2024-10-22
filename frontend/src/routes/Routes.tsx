a42c1a6fa9abdb59eca36564fb478c009d34a7ac
ashik-ad <dev.ashikdhimal@gmail.com>
2024-10-22 23:45:26 +0545

    removed auth routes

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,44 +1,13 @@
-import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import NotFound from "../page/404/NotFound";
 import Store from "../store/Store";
-import Spinner from "../components/loading/Spinner";
 import Navbar from "../components/nav/navbar";
 import WithUser from "../with-user/with-user";
 import Home from "../page/home/Home";
-const SignUp = React.lazy(() => import("../page/signup/SignUp"));
-const Login = React.lazy(() => import("../page/login/Login"));
-const Landing = React.lazy(() => import("../page/landing/Landing"));
-const AppRoutes: React.FC = () => {
+
+const AppRoutes = () => {
   return (
     <Routes>
-      <Route
-        path="/get-started/"
-        element={
-          <Suspense>
-            <Landing />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/signup/*"
-        element={
-          <Suspense fallback={<Spinner />}>
-            <SignUp />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/login/*"
-        element={
-          <Store>
-            <Suspense fallback={<Spinner />}>
-              <Login />
-            </Suspense>
-          </Store>
-        }
-      />
-
       <Route
         path="/*"
         element={
