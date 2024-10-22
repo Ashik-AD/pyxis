62c8430d5d3277774aa4f1897fd052b4978de08b
ashik-ad <dev.ashikdhimal@gmail.com>
2024-10-22 20:57:16 +0545

    fetch movie list first

--- frontend/src/page/home/Home.tsx
+++ frontend/src/page/home/Home.tsx
@@ -1,24 +1,33 @@
-import React, { FC, useEffect, useContext, Suspense } from "react";
+import React, { FC, useEffect, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import { ax } from "../../config/default";
+import Auth from "../../auth/Auth";
+import useDispatch from "../../hooks/useDispatch";
+import useFetch from "../../hooks/useFetch";
+import useStore from "../../hooks/useStore";
+
 const AllMovieTv = React.lazy(() => import("../all/AllMovieTv"));
 const Movie = React.lazy(() => import("../details/Movie"));
 const Person = React.lazy(() => import("../details/Person"));
 const Tv = React.lazy(() => import("../details/Tv"));
-import Index from "./pages/Index";
 const Search = React.lazy(() => import("../search/Search"));
 const DiscoverByGenre = React.lazy(() => import("../discover/DiscoverByGenre"));
 const CollectionInfo = React.lazy(
   () => import("../../components/collection/CollectionInfo"),
 );
-import { StoreContext } from "../../store/Store";
-import { ax } from "../../config/default";
-import Auth from "../../auth/Auth";
 const Like = React.lazy(() => import("../liked/Like"));
 const WatchList = React.lazy(() => import("../watchList/WatchList"));
 const Users = React.lazy(() => import("../user/index"));
 
 const Home: FC = () => {
-  const { store, dispatch } = useContext(StoreContext);
+  const dispatch = useDispatch();
+  const store = useStore();
+  const { data } = useFetch("/movie/popular");
+
+  if (data && !store.movie_full) {
+    dispatch({ type: "SET_MOVIE_FULL", payload: data });
+  }
 
   useEffect(() => {
     let initialFetch = null;
@@ -35,94 +44,92 @@ const Home: FC = () => {
   }, [store.user, dispatch]);
 
   return (
-    <div className="home">
-      <div className="h-screen w-full relative z-1">
-        <Routes>
-          <Route path="/*" element={<Index />} />
-          <Route
-            path="/movie/info/:id/*"
-            element={
+    <div className="h-screen w-full relative z-1">
+      <Routes>
+        <Route path="/*" element={<Index />} />
+        <Route
+          path="/movie/info/:id/*"
+          element={
+            <Suspense>
+              <Movie />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/tv/info/:id/*"
+          element={
+            <Suspense>
+              <Tv />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/discover/:type/genre/:genre_id/*"
+          element={
+            <Suspense>
+              <DiscoverByGenre />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/liked"
+          element={
+            <Suspense>
+              <Like />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/collection/:id"
+          element={
+            <Suspense>
+              <CollectionInfo />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/watchlist"
+          element={
+            <Suspense>
+              <WatchList />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/person/:person_id"
+          element={
+            <Suspense>
+              <Person />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/all/:type/:src/*"
+          element={
+            <Suspense>
+              <AllMovieTv />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/search/*"
+          element={
+            <Suspense>
+              <Search />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/user/*"
+          element={
+            <Auth>
               <Suspense>
-                <Movie />
+                <Users />
               </Suspense>
-            }
-          />
-          <Route
-            path="/tv/info/:id/*"
-            element={
-              <Suspense>
-                <Tv />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/discover/:type/genre/:genre_id/*"
-            element={
-              <Suspense>
-                <DiscoverByGenre />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/liked"
-            element={
-              <Suspense>
-                <Like />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/collection/:id"
-            element={
-              <Suspense>
-                <CollectionInfo />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/watchlist"
-            element={
-              <Suspense>
-                <WatchList />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/person/:person_id"
-            element={
-              <Suspense>
-                <Person />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/all/:type/:src/*"
-            element={
-              <Suspense>
-                <AllMovieTv />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/search/*"
-            element={
-              <Suspense>
-                <Search />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/user/*"
-            element={
-              <Auth>
-                <Suspense>
-                  <Users />
-                </Suspense>
-              </Auth>
-            }
-          />
-        </Routes>
-      </div>
+            </Auth>
+          }
+        />
+      </Routes>
     </div>
   );
 };
