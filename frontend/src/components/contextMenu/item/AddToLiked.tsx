5c36c8d7da9230293d2c09635894560bbbf7b494
ashik-ad <dev.ashikdhimal@gmail.com>
2024-10-23 22:37:19 +0545

    refactored response and others

--- frontend/src/components/contextMenu/item/AddToLiked.tsx
+++ frontend/src/components/contextMenu/item/AddToLiked.tsx
@@ -1,9 +1,20 @@
-import { FC, useContext, useState } from "react";
+import { useContext, useState } from "react";
 import ShowAlert from "../../alert/ShowAlert";
 import { ItemPropsTypes } from "./itemTypes";
 import { ax } from "../../../config/default";
 import { CollectionContext } from "../../collection/CollectionItemLists";
-const AddToLiked: FC<PropsType> = (props) => {
+import { AxiosError } from "axios";
+
+interface Props extends ItemPropsTypes {
+  item_key: string;
+  posterPath: string;
+  title: string;
+  duration: number;
+  release_date: Date | null;
+  media_type: "tv" | "movie";
+}
+
+const AddToLiked = (props: Props) => {
   const {
     id,
     item_key,
@@ -16,9 +27,11 @@ const AddToLiked: FC<PropsType> = (props) => {
     handleMouseOver,
     uid,
   } = props;
+
   const [success, setSuccess] = useState("");
   const [error, setError] = useState("");
   const ctx = useContext(CollectionContext);
+
   const handleAddLiked = async () => {
     try {
       const prepareLike = {
@@ -29,19 +42,17 @@ const AddToLiked: FC<PropsType> = (props) => {
         release_date,
         media_type,
       };
-      const { data, status } = await ax.post(`/${uid}/liked/add`, {
+      const { data } = await ax.post(`/${uid}/liked/add`, {
         data: prepareLike,
       });
-      if (status === 200) {
-        setError(data);
-        return false;
-      }
+
       ctx.updateLikeStatus(id);
       setSuccess(data.message);
-
-      return true;
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        setError(err.response?.data?.message);
+        return;
+      }
       setError(`Can't add this to like`);
     }
   };
@@ -50,6 +61,7 @@ const AddToLiked: FC<PropsType> = (props) => {
     setSuccess("");
     setError("");
   };
+
   return (
     <div
       className="hover-bg-fade py-10 px-16 rounded-regular"
@@ -64,12 +76,5 @@ const AddToLiked: FC<PropsType> = (props) => {
     </div>
   );
 };
-interface PropsType extends ItemPropsTypes {
-  item_key: string;
-  posterPath: string;
-  title: string;
-  duration: number;
-  release_date: Date | null;
-  media_type: "tv" | "movie" | string;
-}
+
 export default AddToLiked;
